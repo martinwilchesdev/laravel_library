0bcd0a088e4c472db987b94bfa84c6a9a9648d03
Martin Wilches <martindotdev@gmail.com>
2024-09-13 22:13:59 -0500

    Campos añadidos al formulario de creacion y edicion de libros

--- resources/js/Pages/Books/Form.vue
+++ resources/js/Pages/Books/Form.vue
@@ -8,6 +8,7 @@ import NavLink from '@/Components/NavLink.vue'
 
 import { Head, useForm } from '@inertiajs/vue3'
 import { ref } from 'vue'
+import DarkButton from '@/Components/DarkButton.vue'
 
 const props = defineProps({
     authors: {
@@ -32,7 +33,7 @@ const form = useForm({
     publisher: '',
     pages: '',
     image: '',
-    release_date: '',
+    release_day: '',
     authors: [],
 })
 
@@ -42,6 +43,8 @@ const srcImg = ref('../../storage/img/example.jpg')
 
 const classMsj = ref('hidden')
 const msj = ref('')
+
+const save = () => {}
 </script>
 
 <template>
@@ -75,7 +78,7 @@ const msj = ref('')
             <div class="min-w-0 p-4 rounded-lg shadow-xs">
                 <form class="mt-6 mb-6 space-y-6 max-w-xl">
                     <InputGroup
-                        :text="isbn"
+                        :text="'ISBN'"
                         :required="req"
                         v-model="form.isbn"
                     >
@@ -95,10 +98,163 @@ const msj = ref('')
                         </svg>
                     </InputGroup>
                     <InputError :message="form.errors.isbn" />
+                    <InputGroup
+                        :text="'Description'"
+                        :required="req"
+                        v-model="form.description"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="size-6"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+                            />
+                        </svg>
+                    </InputGroup>
+                    <InputError :message="form.errors.description" />
+                    <InputGroup
+                        :text="'Title'"
+                        :required="req"
+                        v-model="form.title"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="size-6"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
+                            />
+                        </svg>
+                    </InputGroup>
+                    <InputError :message="form.errors.title" />
+                    <InputGroup
+                        :text="'Publisher'"
+                        :required="req"
+                        v-model="form.publisher"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="size-6"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z"
+                            />
+                        </svg>
+                    </InputGroup>
+                    <InputError :message="form.errors.publisher" />
+                    <InputGroup
+                        :text="'Release date'"
+                        :required="req"
+                        :type="'date'"
+                        v-model="form.release_date"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="size-6"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
+                            />
+                        </svg>
+                    </InputGroup>
+                    <InputError :message="form.errors.release_date" />
+                    <InputGroup
+                        :text="'Pages'"
+                        :required="req"
+                        :type="'number'"
+                        v-model="form.pages"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="size-6"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
+                            />
+                        </svg>
+                    </InputGroup>
+                    <InputGroup
+                        :text="'Image'"
+                        :required="req"
+                        :type="'file'"
+                        :accept="'image/*'"
+                        v-model="form.image"
+                        v-if="props.book"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="size-6"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+                            />
+                        </svg>
+                    </InputGroup>
+                    <InputGroup
+                        :text="'Image'"
+                        :type="'file'"
+                        :accept="'image/*'"
+                        v-model="form.image"
+                        v-else
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="size-6"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+                            />
+                        </svg>
+                    </InputGroup>
+                    <InputError :message="form.errors.image" />
+                    <PrimaryButton @click="save">Save</PrimaryButton>
                 </form>
             </div>
             <div class="min-w-0 p-4 rounded-lg shadow-xs">
-                <img :src="srcImg" :alt="book.title" width="200px" />
+                <img :src="srcImg" :alt="book.title" width="250px" />
             </div>
         </div>
     </AuthenticatedLayout>
